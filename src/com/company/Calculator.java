7649cd4cd34547ca13c512181ae6b158c3c55a79
Misha Isopesku <78026214+MishaIsopesku@users.noreply.github.com>
2022-05-22 12:42:41 +0300

    Delete src/com/company directory

--- src/com/company/Calculator.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface Calculator {
-    int calculateTotal(CoinBundle enteredCoins);
-    CoinBundle calculateChange(int enteredByUserMoney);
-}
--- src/com/company/VendingMachineController.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface VendingMachineController {
-
-    CoinBundle calculateChange(VendingMachineRequest request);
-}
